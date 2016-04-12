1160977060a01bc6b62da007425f09a4c6cf8dc6
Toan Pham <reset.pointer@gmail.com>
2016-04-12 14:46:12 -0400

    good version w/ aufs patch

---
+++ build.sh
@@ -0,0 +1,2 @@
+make ARCH=arm CROSS_COMPILE=/media/samsung/t2-trunk-from-t2-svn.dev/build/ngss_glibc-9.0-trunk-ngss_glibc-arm-gnueabi-cortex-a8-cross-linux/TOOLCHAIN/tools.cross/crosscc/arm-t2-linux-gnueabi- uImage
+
---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Tue Apr 12 13:51:17 EDT 2016"
+#define LINUX_COMPILE_TIME "13:51:17"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILE_DOMAIN "localnet"
+#define LINUX_COMPILER "gcc version 4.5.1 (GCC) "
